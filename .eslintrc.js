39800d1af4275e885883c03504beb9de16241c12
Mark Vayngrib <mark@tradle.io>
2017-11-08 23:26:25 -0500

    stop validating against message interface (which is being phased out)

--- .eslintrc.js
+++ .eslintrc.js
@@ -188,6 +188,7 @@ module.exports = {
         "no-unmodified-loop-condition": "error",
         "no-unneeded-ternary": "error",
         "no-unused-expressions": "error",
+        "no-unused-vars": "warn",
         "no-use-before-define": "off",
         "no-useless-call": "error",
         "no-useless-computed-key": "error",
